833ff485c07843a35f90bf622afdf34aab6d5688
guoguolan1991 <guoguolan1991@gmail.com>
2017-08-08 10:53:40 +0800

    fix the font error

---
+++ ch.py
@@ -0,0 +1,8 @@
+# -*-coding:utf-8-*-
+__author__ = 'lanweiguo'
+
+
+def set_ch():
+    from pylab import mpl
+    mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
+    mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
--- extract_keyword.py
+++ extract_keyword.py
@@ -16,3 +16,41 @@ print count
 
 for tag in tags:
     print tag[0], tag[1]
+    #print tag
+
+from os import path
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator
+import ch
+ch.set_ch()
+
+d = path.dirname(__file__)
+
+alice_coloring = imread(path.join(d, "data/timg.png"))
+
+wc = WordCloud(background_color="white",
+mask=alice_coloring,
+max_font_size=40,
+font_path='C:/Windows/fonts/STFANGSO.TTF',
+random_state=42)
+#wc.generate(text)
+items = {}
+for tag in tags:
+    items[tag[0]] = tag[1]
+
+wc.generate_from_frequencies(items)
+image_colors = ImageColorGenerator(alice_coloring)
+
+plt.imshow(wc)
+plt.axis("off")
+plt.figure()
+# recolor wordcloud and show
+# we could also give color_func=image_colors directly in the constructor
+plt.imshow(wc.recolor(color_func=image_colors))
+plt.axis("off")
+plt.figure()
+plt.imshow(alice_coloring, cmap=plt.cm.gray)
+plt.axis("off")
+plt.show()
+wc.to_file(path.join(d, "zhanlang2.png"))
